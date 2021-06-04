e3bf9e493c72d89c2b2b0688c29651db3fdffa4d
Atik Hasan <atikhasan801@gmail.com>
2021-06-04 14:56:03 +0600

    first commit

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import Category1 from '../Category1/Category1';
-import HomeSlider from '../HomeSlider/HomeSlider'
-import './Homepage.css';
+import React from "react";
+import Category1 from "../Category1/Category1";
+import HomeSlider from "../HomeSlider/HomeSlider";
+import "./Homepage.css";
+import Category2 from "../Category2/Category2";
 
 const Homepage = () => {
-    return (
-        <div className="home">
-            <div className="container">
-                <HomeSlider />
-            </div>
-            <Category1/>
+  return (
+    <>
+      <div className="home">
+        <div className="container">
+          <HomeSlider />
         </div>
-    )
-}
+        <Category1 />
+      </div>
+
+      <div>
+        <Category2 />
+      </div>
+    </>
+  );
+};
 
 export default Homepage;
