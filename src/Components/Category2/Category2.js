86d42e6b027219ec57d087997de7cc7d84aa74b6
Shajib <sajidislam729@gmail.com>
2021-06-04 16:16:45 +0600

    category 2 modified

--- src/Components/Category2/Category2.js
+++ src/Components/Category2/Category2.js
@@ -4,11 +4,11 @@ import "./Category2.css";
 const Category2 = () => {
   return (
     <>
-      <div className="mainDiv">
+      <div className="mainDiv container">
         <div className="rectangle">
           <h5 className="category">Category 2</h5>
           <div className="row">
-            <div className="col-4">
+            <div className="col-lg-4 col-12">
               <div className="item-1">
                 <p>{new Date().toDateString()}</p>
                 <h6>
@@ -18,7 +18,7 @@ const Category2 = () => {
               </div>
             </div>
 
-            <div className="col-4">
+            <div className="col-lg-4 col-12">
               <div className="item-1 item-2">
                 <p>{new Date().toDateString()}</p>
                 <h6>
@@ -28,7 +28,7 @@ const Category2 = () => {
               </div>
             </div>
 
-            <div className="col-4">
+            <div className="col-lg-4">
               <div className="item-1 item-3">
                 <p>{new Date().toDateString()}</p>
                 <h6>
