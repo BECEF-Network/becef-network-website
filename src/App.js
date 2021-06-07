116f296a67f3d3daf413669a82fe17cf2b0aa245
Shajib <sajidislam729@gmail.com>
2021-06-07 12:40:20 +0600

    search bar create

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import ContactPage from "./pages/Contact/Contactpage";
 import HomePage from "./pages/Homepage/Homepage";
 import Aboutus from './Components/aboutus/Aboutus';
+import Footer from "./Components/Footer/Footer";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/contact" component={ContactPage} />
         <Route path="/about" component={Aboutus}/>
       </Switch>
+      <Footer/>
     </Router>
   );
 }
