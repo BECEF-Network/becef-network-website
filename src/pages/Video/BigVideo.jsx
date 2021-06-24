195f59e4b8bd05b3358d9128ae66a303ae8f92c0
Shajib <sajidislam729@gmail.com>
2021-06-24 21:50:57 +0600

    .

--- src/pages/Video/BigVideo.jsx
+++ src/pages/Video/BigVideo.jsx
@@ -8,7 +8,7 @@ function BigVideo({videos}) {
     return (
         <div>
             <div className="row">
-            <div className="col-md-8">
+            <div className="col-lg-8">
                 {/* <div className="post"> */}
                     {
                         videos.slice(0,1).map((video,i) => {
@@ -20,7 +20,7 @@ function BigVideo({videos}) {
                     }
                 {/* </div> */}
             </div>
-            <div className="col-md-4 mh-100">
+            <div className="col-lg-4 mh-100">
                 <div className="row h-100">
                     {
                         videos.slice(1,3).map((video,i) => {
@@ -38,7 +38,7 @@ function BigVideo({videos}) {
         <div className="row post-4">
             {
                 videos.slice(3,6).map((video,i) => {
-                    return <div className="col-lg-4 p-2 py-3">
+                    return <div className="col-4 p-2 py-3">
                                 <div className="small-video-post2" style={{background:`linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7)),url(${video.img}) center no-repeat`}}>
                                     <span className="playIcon"><FontAwesomeIcon icon={faPlayCircle}/></span>
                                     <h1>{video.title}</h1>
