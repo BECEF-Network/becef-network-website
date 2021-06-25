49b5d4170d7fcf0e17fa21ad210ebad796bb1a39
Shajib <sajidislam729@gmail.com>
2021-06-25 15:33:53 +0600

    video slider modified

--- src/pages/Video/SliderVideo.jsx
+++ src/pages/Video/SliderVideo.jsx
@@ -9,37 +9,40 @@ import { faPlayCircle } from '@fortawesome/free-regular-svg-icons'
 
 // Import Swiper styles
 import "swiper/swiper.min.css";
+import "swiper/components/effect-coverflow/effect-coverflow.min.css"
 import "swiper/components/pagination/pagination.min.css"
 
 
 // import Swiper core and required modules
 import SwiperCore, {
-  Pagination
+  EffectCoverflow,Pagination
 } from 'swiper/core';
 
 // install Swiper modules
-SwiperCore.use([Pagination]);
+SwiperCore.use([EffectCoverflow,Pagination]);
 
 
 export default function SliderVideo({videos}) {
   
-  console.log(videos.slice(9,15).map(video=>video.title));
-  
   return (
     <div className="container">
         <hr/>
-        <Swiper slidesPerView={'auto'} centeredSlides={true} loop={true} spaceBetween={30}     grabCursor={true} pagination={{
-        "clickable": true
-        }} className="mySwiper">
+        <Swiper loop={true} spaceBetween={30} effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={'auto'} coverflowEffect={{
+        "rotate": 40,
+        "stretch": 0,
+        "depth": 0,
+        "modifier": 1,
+        "slideShadows": true
+      }} pagination={true} className="mySwiper slider-video">
                     {
-                        videos.slice(9,15).map((video)=>{
-                            return <SwiperSlide>
-                                    <div style={{background:`linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7)),url(${video.img}) center no-repeat`}} className="slider_video_post">
-                                        <span className="playIcon"><FontAwesomeIcon icon={faPlayCircle}/></span>
-                                        <h5>{video.title }</h5>
-                                    </div>
-                                   </SwiperSlide>
-                        })
+                      videos.slice(9,15).map((video)=>{
+                          return <SwiperSlide>
+                                  <div style={{background:`linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.5)),url(${video.img}) center no-repeat`}} className="slider_video_post">
+                                      <span className="playIcon"><FontAwesomeIcon icon={faPlayCircle}/></span>
+                                      <h5>{video.title }</h5>
+                                  </div>
+                                  </SwiperSlide>
+                      })
                     }
         </Swiper>
     </div>
